407d208b9174914f548d630a3f889e4dd3158813
ptahv <petri.tahvanainen@enersoft.fi>
2018-11-26 15:39:13 +0200

    Added bg-secondary for Page.Info-component. Path fix for bs-no-important

--- packages/komponents/src/components/Page/Info/Info.jsx
+++ packages/komponents/src/components/Page/Info/Info.jsx
@@ -7,7 +7,7 @@ export default ({
     children
 }) => (
 	<div 
-		className={'w-100 px-3 py-2 position-relative ' + className}
+		className={'w-100 px-3 py-2 position-relative bg-secondary ' + className}
 		style={style}
 		>
 		{children}
